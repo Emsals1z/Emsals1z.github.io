0a77593431eea94408a9a0552d62c4f416ea0bed
Emsalsiz <kayra2662004@hotmail.com>
2024-07-14 04:53:22 +0300

    Update image.js

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,7 +1,7 @@
 
 window.onload=function(){
 
-        for (let i = 0; i < 300; i++) {
+        for (let i = 0; i < 800; i++) {
            
            
             $(".resimler").append(`<img src="https://assets.nopixel.net/dev/images/clothing/Jacket/mp_f_freemode_01/${i}.webp" class="resim" alt="">`)
