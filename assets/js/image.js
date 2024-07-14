f682f94aaba72aef6db30064e71970260c530094
Emsalsiz <kayra2662004@hotmail.com>
2024-07-14 04:58:46 +0300

    Update image.js

--- assets/js/image.js
+++ assets/js/image.js
@@ -2,15 +2,15 @@
 window.onload=function(){
 
         for (let i = 0; i < 800; i++) {
-           
+         
            
             $(".resimler").append(`<img src="https://assets.nopixel.net/dev/images/clothing/Jacket/mp_f_freemode_01/${i}.webp" class="resim" alt="">`)
-
+   console.log("Yüklendi"+i)
          
         }
  
    
-console.log("JAVASCRİPT YÜKLENDİ 2")
+console.log("JAVASCRİPT YÜKLENDİ 3")
 }
 
 
